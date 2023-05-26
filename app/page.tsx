03dd043137933ec2ce140e8d4b65dbebe6ce6049
xWxfFle <factores175@gmail.com>
2023-05-26 20:56:48 +0300

    Another ios fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 import { About } from '@/components/dom/About'
 import { Landing } from '@/components/dom/Landing'
-import Script from 'next/script'
 
 export default function Page() {
   return (
     <>
       <Landing />
       <About />
-      <Script src='inobounce.js'/>
     </>
   )
 }
